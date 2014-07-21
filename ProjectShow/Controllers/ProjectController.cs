9d264af0f9c0504bddda702cbfa781eafe48aead
aicike <176534021@qq.com>
2014-07-21 12:01:23 +0800

    add

--- ProjectShow/Controllers/ProjectController.cs
+++ ProjectShow/Controllers/ProjectController.cs
@@ -17,15 +17,16 @@ namespace ProjectShow.Controllers
         public ActionResult Index(int? id)
         {
             ProjectModel pmodel = new ProjectModel();
-            var list = pmodel.GetProjectByEID(1).ToPagedList(id ?? 1, 15);
+            var list = pmodel.GetProjectByEID(LoginAccount.EnterpriseID).ToPagedList(id ?? 1, 15);
+            ViewBag.Menu = 2;
             return View(list);
         }
 
         public ActionResult Add()
         {
+            ViewBag.Menu = 2;
             return View();
         }
-
 
         [HttpPost]
         public ActionResult Add(Project project)
@@ -41,6 +42,25 @@ namespace ProjectShow.Controllers
             return JavaScript("window.location.href='" + Url.Action("Index", "Project") + "'");
         }
 
+        public ActionResult Edit(int id)
+        {
+            ProjectModel pmodel = new ProjectModel();
+            var obj = pmodel.Get(id);
+            ViewBag.Menu = 2;
+            return View(obj);
+        }
 
+        [HttpPost]
+        public ActionResult Edit(Project project)
+        {
+            ProjectModel pmodel = new ProjectModel();
+            project.EnterpriseID = LoginAccount.EnterpriseID;
+            var result = pmodel.Edit(project);
+            if (result.HasError)
+            {
+                return Alert(result);
+            }
+            return JavaScript("window.location.href='" + Url.Action("Index", "Project") + "'");
+        }
     }
 }
